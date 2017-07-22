949d2f3530464b361ae5727299f06dc58e634905
ZZF <zzf950307@163.com>
2017-07-22 19:25:03 +0800

    将stateChanger更改为reducer,数据放入reducer中，reducer为纯函数

--- src/index.js
+++ src/index.js
@@ -1,42 +1,46 @@
-const appState = {
-    title: {
-        text: '学习React',
-        color: 'red'
-    },
-    content: {
-        text: 'React内容',
-        color: 'blue'
-    }
-}
-function renderApp(newAppState,oldAppState={}) {
+function renderApp(newAppState, oldAppState = {}) {
     // 判断数据是否改变，改变后重新渲染
-    if(newAppState === oldAppState) return
-    renderTitle(newAppState.title,oldAppState.title)
-    renderContent(newAppState.content,oldAppState.content)
+    if (newAppState === oldAppState) 
+        return
+    renderTitle(newAppState.title, oldAppState.title)
+    renderContent(newAppState.content, oldAppState.content)
 }
 
-function renderTitle(title,oldTitle = {}) {
-    if(title === oldTitle) return
+function renderTitle(title, oldTitle = {}) {
+    if (title === oldTitle) 
+        return
     const titleDOM = document.getElementById('title')
     titleDOM.innerHTML = title.text
     titleDOM.style.color = title.color
 }
 
-function renderContent(content,oldContent={}) {
-    if(content === oldContent) return
+function renderContent(content, oldContent = {}) {
+    if (content === oldContent) 
+        return
     const contentDOM = document.getElementById('content')
     contentDOM.innerHTML = content.text
     contentDOM.style.color = content.color
 }
-
-function stateChanger(state, action) {
+// reducer是一个纯函数 不能在里面修改DOM，也不能发送AJAX请求
+function reducer(state, action) {
+    if (!state) {
+        return {
+            title: {
+                text: '学习React',
+                color: 'red'
+            },
+            content: {
+                text: 'React内容',
+                color: 'blue'
+            }
+        }
+    }
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            // 使用解构赋值，实现复制对象到一个新的对象中，
-            // 同时保留相对应的未改变的数据
+            // 使用解构赋值，实现复制对象到一个新的对象中， 同时保留相对应的未改变的数据
             return {
                 ...state,
-                title:{
+                title: {
                     ...state.title,
                     text: action.text
                 }
@@ -44,40 +48,41 @@ function stateChanger(state, action) {
         case 'UPDATE_TITLE_COLOR':
             return {
                 ...state,
-                totle:{
+                totle: {
                     ...state.title,
-                    color:action.color
+                    color: action.color
                 }
             }
-        // 返回原state对象
+            // 返回原state对象
         default:
             return state
     }
 }
 
-// 抽离 dispatch 方法，并且将数据的获取放入此方法
-// 增加subscribe 的函数，
-function createStore(state, stateChanger) {
+// 抽离 dispatch 方法，并且将数据的获取放入此方法 增加subscribe 的函数，
+function createStore(reducer) {
     const listeners = []
+    let state = null
     // 将参数renderApp 放入数组中。
-    const subscribe = (listener)=>listeners.push(listener)
-    const getState = ()=> (state)
+    const subscribe = (listener) => listeners.push(listener)
+    const getState = () => (state)
     const dispatch = (action) => {
-        // 再此调用stateChanger函数，同时更改state
-        state = stateChanger(state, action)
+        // 再此调用reducer函数，同时更改state
+        state = reducer(state, action)
         //循环执行数组中的renderApp函数
-        listeners.forEach((listener)=>listener())
+        listeners.forEach((listener) => listener())
     }
-    return {getState, dispatch,subscribe}
+    dispatch({}) // 在此处初始化state
+    return {getState, dispatch, subscribe}
 }
 
-const store = createStore(appState, stateChanger)
+const store = createStore(reducer)
 let oldState = store.getState() // 缓存获取到旧的state
 // 从createStore中获取state数据
-store.subscribe(()=>{
+store.subscribe(() => {
     // 获取新的state数据并保存
     const newState = store.getState()
-    renderApp(newState,oldState)
+    renderApp(newState, oldState)
     oldState = newState // 完成渲染之后，新的newState 变为旧的oldState,等待下一次数据的变化重新渲染
 })
 
